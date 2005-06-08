0983e7f07f75d5ef472ac60275261edd10a6b367
stevenhartland <stevenhartland@bac25679-d237-0410-bec6-f8029acc7ebe>
2005-06-08 15:49:21 +0000

    Split out packet combining into a seperate package so that it can easily be user by other modules. Added packet combining support to a2s. Note: the pkt_index and pkt_id is currently guess work waiting on the official specs from valve but as it stands it works for double packet responses.

--- trunk/qstat2/a2s.c
+++ trunk/qstat2/a2s.c
@@ -18,6 +18,7 @@
 
 #include "debug.h"
 #include "qstat.h"
+#include "packet_manip.h"
 
 #define A2S_GETCHALLENGE      "\xFF\xFF\xFF\xFF\x57"
 #define A2S_CHALLENGERESPONSE 0x41
@@ -27,7 +28,7 @@
 #define A2S_PLAYER            "\xFF\xFF\xFF\xFF\x55"
 #define A2S_PLAYERRESPONSE    0x44
 #define A2S_RULES             "\xFF\xFF\xFF\xFF\x56"
-#define A2S_RULESRESPONSE     0x45
+#define A2S_RULESRESPONSE_HL 0x45
 
 struct a2s_status
 {
@@ -66,7 +67,7 @@ void send_a2s_rule_request_packet(struct qserver *server)
 
     if(!get_server_rules && !get_player_info)
     {
-	goto error;
+		goto error;
     }
 
     do
@@ -120,113 +121,170 @@ void deal_with_a2s_packet(struct qserver *server, char *rawpkt, int pktlen)
 
     if(server->server_name == NULL)
     {
-	server->ping_total += time_delta( &packet_recv_time, &server->packet_time1);
-	server->n_requests++;
+		server->ping_total += time_delta( &packet_recv_time, &server->packet_time1);
+		server->n_requests++;
     }
 
-    if(pktlen < 5) goto out_too_short;
+    if( pktlen < 5) goto out_too_short;
 
-    if(memcmp(pkt, "\xFF\xFF\xFF\xFF", 4))
-    {
-	malformed_packet(server, "invalid packet header");
-	goto out_error;
-    }
+    if( 0 == memcmp(pkt, "\xFE\xFF\xFF\xFF", 4) )
+	{
+		// fragmented packet
+		// pkt_id is currently constant
+		// pkt_max is the total number of packets expected
+		// pkt_index is a bit mask of the packets received.
+		unsigned char pkt_index, pkt_max;
+		unsigned int pkt_id = 1;
 
-    pkt += 4;
-    pktlen -= 4;
+		pkt += 8;
 
-    pktlen -= 1;
-    switch(*pkt++)
-    {
+		// next byte is the order byte?
+		//fprintf( stderr, "FRAGGG: %hhx\n", *pkt );
+		switch( *pkt )
+		{
+		case 0x02:
+			pkt_index = 0;
+			break;
+
+		case 0x12:
+			pkt_index = 1;
+			break;
+
+		default:
+			malformed_packet( server, "unknown packet index" );
+			goto out_error;
+		}
+		pkt++;
+
+		SavedData *sdata;
+		pkt_max = 2;
+
+		if ( server->saved_data.data == NULL )
+		{
+			sdata = &server->saved_data;
+		}
+		else
+		{
+			sdata = (SavedData*) calloc( 1, sizeof(SavedData));
+			sdata->next = server->saved_data.next;
+			server->saved_data.next = sdata;
+		}
+		//fprintf( stderr, "FRAG: %d, %d\n", pkt_id, pkt_index );
+
+		sdata->pkt_index = pkt_index;
+		sdata->pkt_max = pkt_max;
+		sdata->pkt_id = pkt_id;
+		sdata->datalen = pktlen - 9;
+		sdata->data= (char*) malloc( sdata->datalen);
+		memcpy( sdata->data, pkt, sdata->datalen);
+
+		// combine_packets will call us recursively
+		combine_packets( server );
+		return;
+	}
+
+	if( 0 != memcmp(pkt, "\xFF\xFF\xFF\xFF", 4) )
+	{
+		malformed_packet(server, "invalid packet header");
+		goto out_error;
+	}
+	pkt += 4;
+	pktlen -= 4;
+	//fprintf( stderr, "PAAAAK: %hhx\n", *pkt );
+	pktlen -= 1;
+	switch(*pkt++)
+	{
 	case A2S_CHALLENGERESPONSE:
-	    if(pktlen < 4) goto out_too_short;
-	    memcpy(&status->challenge, pkt, 4);
-	    // do not count challenge as retry
-	    if(!status->have_challenge && server->retry1 != n_retries)
-	    {
-		++server->retry1;
-		if(server->n_retries)
-		    --server->n_retries;
-	    }
-	    status->have_challenge = 1;
-	    debug(3, "challenge %x", status->challenge);
-	    break;
+		if(pktlen < 4) goto out_too_short;
+		memcpy(&status->challenge, pkt, 4);
+		// do not count challenge as retry
+		if(!status->have_challenge && server->retry1 != n_retries)
+		{
+			++server->retry1;
+			if(server->n_retries)
+			{
+				--server->n_retries;
+			}
+		}
+		status->have_challenge = 1;
+		debug(3, "challenge %x", status->challenge);
+		break;
 
 	case A2S_INFORESPONSE_HL1:
-	    //if(pktlen < 1) goto out_too_short;
-	    //snprintf(buf, sizeof(buf), "%hhX", *pkt);
-	    //add_rule(server, "protocol", buf, 0);
-	    //++pkt; --pktlen;
+		//if(pktlen < 1) goto out_too_short;
+		//snprintf(buf, sizeof(buf), "%hhX", *pkt);
+		//add_rule(server, "protocol", buf, 0);
+		//++pkt; --pktlen;
 
-	    // ip:port
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    //server->server_name = strdup(pkt);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
+		// ip:port
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		//server->server_name = strdup(pkt);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
 
-	    // server name
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->server_name = strdup(pkt);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
+		// server name
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		server->server_name = strdup(pkt);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
-	    // map
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->map_name = strdup(pkt);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
+		// map
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		server->map_name = strdup(pkt);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
-	    // mod
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->game = strdup(pkt);
-	    add_rule(server, "gamedir", pkt, 0);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
+		// mod
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		server->game = strdup(pkt);
+		add_rule(server, "gamedir", pkt, 0);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
-	    // description
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    add_rule(server, "gamename", pkt, 0);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
+		// description
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		add_rule(server, "gamename", pkt, 0);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
-	    if(pktlen < 9) goto out_too_short;
+		if(pktlen < 9) goto out_too_short;
 
-	    server->num_players = pkt[0];
-	    server->max_players = pkt[1];
+		server->num_players = pkt[0];
+		server->max_players = pkt[1];
 
 		// version
-	    //add_rule(server, "version", atoi( pkt[2] ) );
+		//add_rule(server, "version", atoi( pkt[2] ) );
 
 		// dedicated
-	    add_rule(server, "dedicated", pkt[3] == 'd' ? "1" : "0", 0);
+		add_rule(server, "dedicated", pkt[3] == 'd' ? "1" : "0", 0);
 
 		// os
-	    if(pkt[4] == 'l')
+		if(pkt[4] == 'l')
 		{
 			add_rule(server, "sv_os", "linux", 0);
 		}
-	    else if(pkt[4] == 'w')
+		else if(pkt[4] == 'w')
 		{
 			add_rule(server, "sv_os", "windows", 0);
 		}
-	    else
-	    {
+		else
+		{
 			buf[0] = pkt[4];
 			buf[1] = '\0';
 			add_rule(server, "sv_os", buf, 0);
-	    }
+		}
 
 		// password
 		add_rule(server, "password", pkt[5] ? "1" : "0" , 0);
 
-	    pkt += 6;
-	    pktlen -= 6;
+		pkt += 6;
+		pktlen -= 6;
 
 		// mod info
 		if ( pkt[0] )
@@ -282,212 +340,217 @@ void deal_with_a2s_packet(struct qserver *server, char *rawpkt, int pktlen)
 		// Version
 		//add_rule(server, "bots", atoi( *pkt ) , 0);
 
-	    status->have_info = 1;
+		status->have_info = 1;
 
-	    server->retry1 = n_retries;
+		server->retry1 = n_retries;
 
-	    server->next_player_info = server->num_players;
+		server->next_player_info = server->num_players;
 
-	    break;
+		break;
 
 	case A2S_INFORESPONSE_HL2:
-	    if(pktlen < 1) goto out_too_short;
-	    snprintf(buf, sizeof(buf), "%hhX", *pkt);
-	    add_rule(server, "protocol", buf, 0);
-	    ++pkt; --pktlen;
+		if(pktlen < 1) goto out_too_short;
+		snprintf(buf, sizeof(buf), "%hhX", *pkt);
+		add_rule(server, "protocol", buf, 0);
+		++pkt; --pktlen;
 
-	    // server name
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->server_name = strdup(pkt);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
-
-	    // map
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->map_name = strdup(pkt);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
-
-	    // mod
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    server->game = strdup(pkt);
-	    add_rule(server, "gamedir", pkt, 0);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
-
-	    // description
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    add_rule(server, "gamename", pkt, 0);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
-
-	    if(pktlen < 9) goto out_too_short;
-
-	    // pkt[0], pkt[1] steam id unused
-	    server->num_players = pkt[2];
-	    server->max_players = pkt[3];
-	    // pkt[4] number of bots
-	    add_rule(server, "dedicated", pkt[5]?"1":"0", 0);
-	    if(pkt[6] == 'l')
-		add_rule(server, "sv_os", "linux", 0);
-	    else if(pkt[6] == 'w')
-		add_rule(server, "sv_os", "windows", 0);
-	    else
-	    {
-		buf[0] = pkt[6];
-		buf[1] = '\0';
-		add_rule(server, "sv_os", buf, 0);
-	    }
-
-	    if(pkt[7])
-	    {
-		snprintf(buf, sizeof(buf), "%u", (unsigned)pkt[7]);
-		add_rule(server, "password", buf, 0);
-	    }
-
-	    if(pkt[8])
-	    {
-		snprintf(buf, sizeof(buf), "%u", (unsigned)pkt[8]);
-		add_rule(server, "secure", buf, 0);
-	    }
-
-	    pkt += 9;
-	    pktlen -= 9;
-
-	    // version
-	    str = memchr(pkt, '\0', pktlen);
-	    if(!str) goto out_too_short;
-	    add_rule(server, "version", pkt, 0);
-	    pktlen -= str-pkt+1;
-	    pkt += str-pkt+1;
-
-	    status->have_info = 1;
-
-	    server->retry1 = n_retries;
-
-	    server->next_player_info = server->num_players;
-
-	    break;
-
-	case A2S_RULESRESPONSE:
-
-	    if(pktlen < 2) goto out_too_short;
-
-	    cnt = pkt[0] + (pkt[1]<<8);
-	    pktlen -= 2;
-	    pkt += 2;
-
-	    debug(3, "num_rules: %d", cnt);
-
-	    for(;cnt && pktlen > 0; --cnt)
-	    {
-		char* key, *value;
+		// server name
 		str = memchr(pkt, '\0', pktlen);
-		if(!str) break;
-		key = pkt;
+		if(!str) goto out_too_short;
+		server->server_name = strdup(pkt);
 		pktlen -= str-pkt+1;
 		pkt += str-pkt+1;
 
+		// map
 		str = memchr(pkt, '\0', pktlen);
-		if(!str) break;
-		value = pkt;
+		if(!str) goto out_too_short;
+		server->map_name = strdup(pkt);
 		pktlen -= str-pkt+1;
 		pkt += str-pkt+1;
 
-		add_rule(server, key, value, NO_FLAGS);
-	    }
+		// mod
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		server->game = strdup(pkt);
+		add_rule(server, "gamedir", pkt, 0);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
 
-	    if(cnt)
-	    {
-		malformed_packet(server, "packet contains too few rules, missing %d", cnt);
-		server->missing_rules = 1;
-	    }
-	    if(pktlen)
+		// description
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		add_rule(server, "gamename", pkt, 0);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
+
+		if(pktlen < 9) goto out_too_short;
+
+		// pkt[0], pkt[1] steam id unused
+		server->num_players = pkt[2];
+		server->max_players = pkt[3];
+		// pkt[4] number of bots
+		add_rule(server, "dedicated", pkt[5]?"1":"0", 0);
+		if(pkt[6] == 'l')
+		{
+			add_rule(server, "sv_os", "linux", 0);
+		}
+		else if(pkt[6] == 'w')
+		{
+			add_rule(server, "sv_os", "windows", 0);
+		}
+		else
+		{
+			buf[0] = pkt[6];
+			buf[1] = '\0';
+			add_rule(server, "sv_os", buf, 0);
+		}
+
+		if(pkt[7])
+		{
+			snprintf(buf, sizeof(buf), "%u", (unsigned)pkt[7]);
+			add_rule(server, "password", buf, 0);
+		}
+
+		if(pkt[8])
+		{
+			snprintf(buf, sizeof(buf), "%u", (unsigned)pkt[8]);
+			add_rule(server, "secure", buf, 0);
+		}
+
+		pkt += 9;
+		pktlen -= 9;
+
+		// version
+		str = memchr(pkt, '\0', pktlen);
+		if(!str) goto out_too_short;
+		add_rule(server, "version", pkt, 0);
+		pktlen -= str-pkt+1;
+		pkt += str-pkt+1;
+
+		status->have_info = 1;
+
+		server->retry1 = n_retries;
+
+		server->next_player_info = server->num_players;
+
+		break;
+
+	case A2S_RULESRESPONSE_HL:
+
+		if(pktlen < 2) goto out_too_short;
+
+		cnt = pkt[0] + (pkt[1]<<8);
+		pktlen -= 2;
+		pkt += 2;
+
+		debug(3, "num_rules: %d", cnt);
+
+		for(;cnt && pktlen > 0; --cnt)
+		{
+			char* key, *value;
+			str = memchr(pkt, '\0', pktlen);
+			if(!str) break;
+			key = pkt;
+			pktlen -= str-pkt+1;
+			pkt += str-pkt+1;
+
+			str = memchr(pkt, '\0', pktlen);
+			if(!str) break;
+			value = pkt;
+			pktlen -= str-pkt+1;
+			pkt += str-pkt+1;
+
+			add_rule(server, key, value, NO_FLAGS);
+		}
+
+		if(cnt)
+		{
+			malformed_packet(server, "packet contains too few rules, missing %d", cnt);
+			server->missing_rules = 1;
+		}
+		if(pktlen)
 		malformed_packet(server, "garbage at end of rules, %d bytes left", pktlen);
 
-	    status->have_rules = 1;
+		status->have_rules = 1;
 
-	    server->retry1 = n_retries;
+		server->retry1 = n_retries;
 
-	    break;
+		break;
 
 	case A2S_PLAYERRESPONSE:
 
-	    if(pktlen < 1) goto out_too_short;
+		if(pktlen < 1) goto out_too_short;
 
-	    cnt = pkt[0];
-	    pktlen -= 1;
-	    pkt += 1;
+		cnt = pkt[0];
+		pktlen -= 1;
+		pkt += 1;
 
-	    debug(3, "num_players: %d", cnt);
+		debug(3, "num_players: %d", cnt);
 
-	    for(;cnt && pktlen > 0; --cnt)
-	    {
-		unsigned idx;
-		const char* name;
-		struct player* p;
-
-		idx = *pkt++;
-		--pktlen;
-
-		str = memchr(pkt, '\0', pktlen);
-		if(!str) break;
-		name = pkt;
-		pktlen -= str-pkt+1;
-		pkt += str-pkt+1;
-
-		if(pktlen < 8) goto out_too_short;
-
-		debug(3, "player index %d", idx);
-		p = add_player(server, server->n_player_info);
-		if(p)
+		for(;cnt && pktlen > 0; --cnt)
 		{
-		    p->name = strdup(name);
-		    p->frags = swap_long_from_little(pkt);
-		    p->connect_time = swap_float_from_little(pkt+4);
+			unsigned idx;
+			const char* name;
+			struct player* p;
+
+			idx = *pkt++;
+			--pktlen;
+
+			str = memchr(pkt, '\0', pktlen);
+			if(!str) break;
+			name = pkt;
+			pktlen -= str-pkt+1;
+			pkt += str-pkt+1;
+
+			if(pktlen < 8) goto out_too_short;
+
+			debug(3, "player index %d", idx);
+			p = add_player(server, server->n_player_info);
+			if(p)
+			{
+				p->name = strdup(name);
+				p->frags = swap_long_from_little(pkt);
+				p->connect_time = swap_float_from_little(pkt+4);
+			}
+			pktlen -= 8;
+			pkt += 8;
 		}
-		pktlen -= 8;
-		pkt += 8;
-	    }
 
 #if 0 // seems to be a rather normal condition
-	    if(cnt)
-	    {
-		malformed_packet(server, "packet contains too few players, missing %d", cnt);
-	    }
+		if(cnt)
+		{
+			malformed_packet(server, "packet contains too few players, missing %d", cnt);
+		}
 #endif
-	    if(pktlen)
-		malformed_packet(server, "garbage at end of player info, %d bytes left", pktlen);
+		if(pktlen)
+			malformed_packet(server, "garbage at end of player info, %d bytes left", pktlen);
 
-	    status->have_player = 1;
+		status->have_player = 1;
 
-	    server->retry1 = n_retries;
+		server->retry1 = n_retries;
 
-	    break;
+		break;
 
-	default:
-	    malformed_packet(server, "invalid packet id %d", *pkt);
-	    goto out_error;
-    }
+		default:
+			malformed_packet(server, "invalid packet id %hhx", *--pkt);
+			goto out_error;
+	}
 
     if((!get_player_info || (get_player_info && status->have_player))
-    && (!get_server_rules || (get_server_rules && status->have_rules)))
+		&& (!get_server_rules || (get_server_rules && status->have_rules))
+	)
     {
-	server->next_rule = NULL;
+		server->next_rule = NULL;
     }
 
-    cleanup_qserver(server, 0);
-    return;
+	cleanup_qserver(server, 0);
+	return;
 
 out_too_short:
-    malformed_packet(server, "packet too short");
+	malformed_packet(server, "packet too short");
 
 out_error:
-    cleanup_qserver(server, 1);
+	cleanup_qserver(server, 1);
 }
 
 // vim: sw=4 ts=8 noet
