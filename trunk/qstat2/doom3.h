93236d54fe8929412e1a50e23abf10d082f1f8e1
l-n <l-n@bac25679-d237-0410-bec6-f8029acc7ebe>
2005-10-07 12:54:10 +0000

    - initial Quake4 support. Thanks to Timothee Besset for providing sample code.

--- trunk/qstat2/doom3.h
+++ trunk/qstat2/doom3.h
@@ -2,7 +2,7 @@
  * qstat 2.9
  * by Steve Jankowski
  *
- * Doom3 protocol
+ * Doom3 / Quake4 protocol
  * Copyright 2005 Ludwig Nussel
  *
  * Licensed under the Artistic License, see LICENSE.txt for license terms
@@ -12,7 +12,14 @@
 
 #include "qstat.h"
 
+#define DOOM3_DEFAULT_PORT	27666
+#define DOOM3_MASTER_DEFAULT_PORT	27650
+
+#define QUAKE4_DEFAULT_PORT	28004
+#define QUAKE4_MASTER_DEFAULT_PORT	27650
+
 char* build_doom3_masterfilter(struct qserver* server, char* buf, unsigned* buflen);
 void deal_with_doom3_packet( struct qserver *server, char *rawpkt, int pktlen);
+void deal_with_quake4_packet( struct qserver *server, char *rawpkt, int pktlen);
 
 #endif
