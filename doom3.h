f708674339b7accc02b75b10535e10c038b56d59
Steven Hartland <steven.hartland@multiplay.co.uk>
2006-06-22 11:13:51 +0000

    Added initial Prey support

--- doom3.h
+++ doom3.h
@@ -18,10 +18,15 @@
 #define QUAKE4_DEFAULT_PORT	28004
 #define QUAKE4_MASTER_DEFAULT_PORT	27650
 
+#define PREY_DEFAULT_PORT	27719
+#define PREY_MASTER_DEFAULT_PORT	27655
+
 void send_doom3master_request_packet( struct qserver *server);
 void deal_with_doom3_packet( struct qserver *server, char *rawpkt, int pktlen);
 
-void deal_with_quake4_packet( struct qserver *server, char *rawpkt, int pktlen);
 void send_quake4master_request_packet( struct qserver *server);
+void deal_with_quake4_packet( struct qserver *server, char *rawpkt, int pktlen);
+
+void deal_with_prey_packet( struct qserver *server, char *rawpkt, int pktlen);
 
 #endif
