3e20d055fa31f9ad4254e25a96a68eee0febe1f9
l-n <l-n@bac25679-d237-0410-bec6-f8029acc7ebe>
2008-01-13 19:32:19 +0000

    ottd support

---
+++ trunk/qstat2/ottd.h
@@ -0,0 +1,20 @@
+/*
+ * qstat 2.11
+ *
+ * opentTTD protocol
+ * Copyright 2007 Ludwig Nussel
+ *
+ * Licensed under the Artistic License, see LICENSE.txt for license terms
+ */
+#ifndef QSTAT_OTTD_H
+#define QSTAT_OTTD_H
+
+#include "qstat.h"
+
+void send_ottdmaster_request_packet(struct qserver *server);
+void deal_with_ottdmaster_packet(struct qserver *server, char *rawpkt, int pktlen);
+
+void send_ottd_request_packet(struct qserver *server);
+void deal_with_ottd_packet(struct qserver *server, char *rawpkt, int pktlen);
+
+#endif
