931529555b9027509b89f8dd01809c88f5bf8354
stevenhartland <stevenhartland@bac25679-d237-0410-bec6-f8029acc7ebe>
2009-12-12 00:26:07 +0000

    Commited midnightBSD patch https://sourceforge.net/tracker/?func=detail&aid=1923095&group_id=56603&atid=481161

--- trunk/qstat2/config.c
+++ trunk/qstat2/config.c
@@ -239,7 +239,7 @@ qsc_load_default_config_files()
     filename= getenv( "HOME");
     if ( filename != NULL && filename[0] != '\0')  {
 	char path[1024];
-	sprintf( path, "%s/%s", filename, HOME_CONFIG_FILE);
+	snprintf( path, (sizeof(path) -1),"%s/%s", var, HOME_CONFIG_FILE);
     }
 /* 1. $QSTAT_CONFIG
    2. UNIX: $HOME/.qstatrc         WIN: $HOME/qstat.cfg
