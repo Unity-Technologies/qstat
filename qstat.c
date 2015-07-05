c3b1a811a2854dc597029c0196c6ba53adb28ade
Thomas Debesse <dev@illwieckz.net>
2015-07-06 01:29:16 +0200

    unset unused variables, ref #19

--- qstat.c
+++ qstat.c
@@ -859,7 +859,7 @@ struct rcv_pkt
 
 void do_work(void)
 {
-	int pktlen, rc, fd;
+	int pktlen, rc;
 	char *pkt = NULL;
 	int bind_retry = 0;
 	struct timeval timeout;
@@ -928,7 +928,6 @@ void do_work(void)
 			break;
 		}
 
-		fd = 0;
 		for (; rc && buffill < bufsize; rc--)
 		{
 			int addrlen = sizeof(buffer[buffill].addr);
@@ -7569,7 +7568,7 @@ static char Dat2Reply1_4[] =
 
 query_status_t deal_with_ghostrecon_packet(struct qserver *server, char *pkt, int pktlen)
 {
-	char str[256], *start, *end, StartFlag, *lpszIgnoreServerPlayer;
+	char str[256], StartFlag, *lpszIgnoreServerPlayer;
 	char *lpszMission;
 	unsigned int iIgnoreServerPlayer, iDedicatedServer, iUseStartTimer;
 	unsigned short GrPayloadLen;
@@ -7584,8 +7583,6 @@ query_status_t deal_with_ghostrecon_packet(struct qserver *server, char *pkt, in
 
 	debug( 2, "deal_with_ghostrecon_packet %p, %d", server, pktlen );
 
-	start = pkt;
-	end = &pkt[pktlen];
 	pkt[pktlen] = '\0';
 
 	/*
