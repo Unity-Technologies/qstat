6bebbaba149d0352ec49a32980dfc4553cc2288b
Steven Hartland <steven.hartland@multiplay.co.uk>
2021-06-18 23:35:17 +0100

    fix(utils): protect against errors in str_replace
    Protect against misuse of str_replace if the replace string is larger
    than the find string by forcing an exit with error.

--- utils.h
+++ utils.h
@@ -8,6 +8,8 @@
 #define QSTAT_UTILS_H
 
 #ifndef _WIN32
+ #include <err.h>
+ #include <sysexits.h>
  #ifdef HAVE_CONFIG_H
 	#include "gnuconfig.h"
  #endif
@@ -41,6 +43,10 @@
 
 #ifndef EX_OSERR
 	#define EX_OSERR    71  /* system error (e.g., can't fork) */
+#endif
+
+#ifndef EX_SOFTWARE
+	#define EX_SOFTWARE 70 /* An internal software error has been detected */
 #endif
 
 #if !HAVE_ERR_H
